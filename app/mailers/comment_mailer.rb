10eb98708711cf489913ce2f9a0d62fb5d669365
BrainFreeze <1briancunningham1@gmail.com>
2015-07-21 12:30:35 -0400

    comment mailer and update feature test

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,22 @@
+class CommentMailer < ApplicationMailer
+  default from: 'bet@ya_betcha.com'
+  def sender_comment(bet, comment)
+    @bet = bet
+    @comment = comment
+    
+    mail(
+      to: bet.sender.email,
+      subject: "New Comment added to #{bet.title}"
+    )
+  end
+
+  def receiver_comment(bet, comment)
+    @bet = bet
+    @comment = comment
+
+    mail(
+      to: bet.sender.email,
+      subject: "New Comment added to #{bet.title}"
+    )
+  end
+end
